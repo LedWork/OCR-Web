809c6958359117e46660d93beef72c3b5ab9a8d2
Jakub Kalinowski <122910419+jkalinowsky@users.noreply.github.com>
2025-03-07 12:56:18 +0100

    Update __init__.py

--- backend/flask-app/app/app/__init__.py
+++ backend/flask-app/app/app/__init__.py
@@ -15,7 +15,6 @@ print(os.getenv("MAIL_SERVER"))
 print(os.getenv("MAIL_PORT"))
 print(os.getenv("MAIL_USERNAME"))
 print(os.getenv("MAIL_PASSWORD"))
-print(os.getenv("MAIL_DEFAULT_SENDER"))
 
 app = Flask(
     __name__,
@@ -59,7 +58,7 @@ app.config['MAIL_SERVER'] = os.getenv("MAIL_SERVER")
 app.config['MAIL_PORT'] = os.getenv('MAIL_PORT')
 app.config['MAIL_USERNAME'] = os.getenv("MAIL_USERNAME")
 app.config['MAIL_PASSWORD'] = os.getenv("MAIL_PASSWORD")
-app.config['MAIL_DEFAULT_SENDER'] = os.getenv("MAIL_DEFAULT_SENDER")
+app.config['MAIL_DEFAULT_SENDER'] = os.getenv("MAIL_USERNAME")
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 
