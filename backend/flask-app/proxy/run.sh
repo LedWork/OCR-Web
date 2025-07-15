71263ca1604946c701179542be24fbb3096141b6
KrzysKond <gaxkk19@gmail.com>
2025-07-15 10:34:22 +0200

    fix hyphen

--- backend/flask-app/proxy/run.sh
+++ backend/flask-app/proxy/run.sh
@@ -7,7 +7,7 @@ CERT_DIR="/etc/letsencrypt/live/${APP_HOST}"
 if [ ! -d "$CERT_DIR" ]; then
   echo "=> First‑time certificate request for ${APP_HOST}"
   certbot certonly --standalone \
-    --non‑interactive \
+    --non-interactive \
     --agree-tos \
     --email "$CERT_EMAIL" \
     -d "$APP_HOST"
