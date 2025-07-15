0582f384ed7ee4682325c0848bb1e50f5e1b4fe1
KrzysKond <gaxkk19@gmail.com>
2025-07-15 10:19:55 +0200

    move certs to container

--- backend/flask-app/proxy/run.sh
+++ backend/flask-app/proxy/run.sh
@@ -3,4 +3,16 @@
 set -e
 envsubst '$${LISTEN_PORT},$${APP_PORT},$${APP_HOST}' < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
 
+CERT_DIR="/etc/letsencrypt/live/${APP_HOST}"
+if [ ! -d "$CERT_DIR" ]; then
+  echo "=> First‑time certificate request for ${APP_HOST}"
+  certbot certonly --standalone \
+    --non‑interactive \
+    --agree-tos \
+    --email "$CERT_EMAIL" \
+    -d "$APP_HOST"
+fi
+
+crond
+
 nginx -g 'daemon off;'
